d9be7127957fe5d83d72488c0e154668eadc976b
Jeremy Freetly <freet8707@gmail.com>
2020-03-20 21:30:21 -0500

    header create function made and appended to HTML

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,6 +2,28 @@
 // -----------------------
 // Using a function create the component you see below:
 //
+function headerCreate(){
+    this.header = document.createElement("div")
+    this.h1 = document.createElement("h1")
+    this.span = document.createElement("span")
+    this.span2 = document.createElement("span")
+
+    this.header.classList.add("header")
+    this.span.classList.add("date")
+    this.span2.classList.add("temp")
+
+    this.header.appendChild(this.span)
+    this.header.appendChild(this.h1)
+    this.header.appendChild(this.span2)
+
+    return this.header
+} 
+
+const headContain = document.querySelector(".header-container")
+headContain.appendChild(headerCreate())
+console.log(headContain)
+// console.log(headerCreate())
+
 //  <div class="header">
 //    <span class="date">SMARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
@@ -9,4 +31,4 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+// function Header() {}
