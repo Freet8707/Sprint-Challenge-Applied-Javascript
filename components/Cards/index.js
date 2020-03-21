e09a79fbb40ee94b7e1aa2145e3f9b1ee811e5fa
Jeremy Freetly <freet8707@gmail.com>
2020-03-20 22:40:56 -0500

    cards created using axios get and then statements

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,72 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+class Card {
+    constructor(headline, url, name){
+        this.cardDiv = document.createElement("div")
+        this.headLineDiv = document.createElement("div")
+        this.authorDiv = document.createElement("div")
+        this.imgDiv = document.createElement("div")
+        this.img = document.createElement("img")
+        this.nameSpan = document.createElement("span")
+
+        this.cardDiv.classList.add("card")
+        this.headLineDiv.classList.add("headline")
+        this.authorDiv.classList.add("author")
+        this.imgDiv.classList.add("img-container")
+
+        this.headLineDiv.textContent = headline
+        this.img.setAttribute("src", url)
+        this.nameSpan.textContent = `By ${name}`
+
+        this.cardDiv.appendChild(this.headLineDiv)
+        this.imgDiv.appendChild(this.img)
+        this.authorDiv.appendChild(this.imgDiv)
+        this.authorDiv.appendChild(this.nameSpan)
+        this.cardDiv.appendChild(this.authorDiv)
+
+        const domAttach = document.querySelector(".cards-container")
+        return domAttach.appendChild(this.cardDiv)
+        // console.log(this.cardDiv)
+    }
+} 
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        console.log(response)
+        return response.data.articles.javascript.forEach((element) => {
+            let javaCard = new Card(element.headline, element.authorPhoto, element.authorName)
+        })
+    })
+    axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        return response.data.articles.bootstrap.forEach((element) => {
+            let bootCard = new Card(element.headline, element.authorPhoto, element.authorName)
+        })
+    })
+    axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        return response.data.articles.technology.forEach((element) => {
+            let techCard = new Card(element.headline, element.authorPhoto, element.authorName)
+        })
+    })
+    axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        return response.data.articles.jquery.forEach((element) => {
+            let jQueryCard = new Card(element.headline, element.authorPhoto, element.authorName)
+        })
+    })
+    axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        return response.data.articles.node.forEach((element) => {
+            let nodeCard = new Card(element.headline, element.authorPhoto, element.authorName)
+        })
+    })
+// let jChan = new Card("Hey", "https://heresjChan.com", "the great jChan")
+
+// console.log(jChan)
